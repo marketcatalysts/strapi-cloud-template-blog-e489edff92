bfab8e20fbc58c709449973b11fe3433ae217188
Kris <kris@marketcatalysts.com>
2024-07-18 19:01:11 -0700

    watch list updates v1

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,107 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface MarketCatalystsCataystsItems extends Schema.Component {
-  collectionName: 'components_market_catalysts_cataysts_items';
-  info: {
-    displayName: 'Cataysts-Items';
-    icon: 'check';
-  };
-  attributes: {
-    marketCatalysts: Attribute.Enumeration<
-      [
-        'featuredCatalysts',
-        'marketCatalysts',
-        'marketMovers',
-        'Earnings',
-        'Economics',
-        'FDA',
-        'Conferences',
-        'IPO',
-        'Dividends',
-        'Splits',
-        'Other'
-      ]
-    >;
-    date: Attribute.Date;
-    description: Attribute.RichText &
-      Attribute.CustomField<
-        'plugin::ckeditor.CKEditor',
-        {
-          output: 'HTML';
-          preset: 'rich';
-        }
-      >;
-  };
-}
-
-export interface MarketCatalystsCatalystSection extends Schema.Component {
-  collectionName: 'components_market_catalysts_catalyst_sections';
-  info: {
-    displayName: 'Catalyst-Section';
-    icon: 'feather';
-  };
-  attributes: {
-    Title: Attribute.String;
-    description: Attribute.RichText &
-      Attribute.CustomField<
-        'plugin::ckeditor.CKEditor',
-        {
-          output: 'HTML';
-          preset: 'rich';
-        }
-      >;
-    catalyst_items: Attribute.Component<
-      'market-catalysts.cataysts-items',
-      true
-    >;
-  };
-}
-
-export interface MarketCatalystsCatalystItem extends Schema.Component {
-  collectionName: 'components_market_catalysts_catalyst_items';
-  info: {
-    displayName: 'Catalyst Item';
-    icon: 'manyWays';
-  };
-  attributes: {
-    type: Attribute.Enumeration<
-      [
-        'Market Catalysts',
-        'Market Movers',
-        'Earnings',
-        'FDA',
-        'Economic',
-        'Conference',
-        'IPO',
-        'Dividend',
-        'Splits',
-        'Other'
-      ]
-    >;
-    date: Attribute.Date;
-    time: Attribute.Time;
-    companyEventName: Attribute.String;
-    tickerSymbol: Attribute.String;
-    description: Attribute.RichText;
-    potentialImpact: Attribute.String;
-    relatedStockSectors: Attribute.String;
-    URL: Attribute.String;
-    importanceLevel: Attribute.Enumeration<
-      [
-        'Extremely High - Stock Market Mover',
-        'High - Individual Stock Mover',
-        'Medium - Potential Stock Mover',
-        'Low - Small Chance Stock Mover'
-      ]
-    >;
-    chartImage: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
-    estimatedValue: Attribute.Decimal;
-    analystsConsensus: Attribute.String;
-    isConfirmed: Attribute.Boolean;
-    tags: Attribute.String;
-  };
-}
-
 export interface SharedSlider extends Schema.Component {
   collectionName: 'components_shared_sliders';
   info: {
@@ -217,12 +115,111 @@ export interface SharedMedia extends Schema.Component {
   };
 }
 
+export interface MarketCatalystsCataystsItems extends Schema.Component {
+  collectionName: 'components_market_catalysts_cataysts_items';
+  info: {
+    displayName: 'Cataysts-Items';
+    icon: 'check';
+  };
+  attributes: {
+    marketCatalysts: Attribute.Enumeration<
+      [
+        'featuredCatalysts',
+        'marketCatalysts',
+        'marketMovers',
+        'Earnings',
+        'Economics',
+        'FDA',
+        'Conferences',
+        'IPO',
+        'Dividends',
+        'Splits',
+        'Other'
+      ]
+    >;
+    date: Attribute.Date;
+    description: Attribute.RichText &
+      Attribute.CustomField<
+        'plugin::ckeditor.CKEditor',
+        {
+          output: 'HTML';
+          preset: 'rich';
+        }
+      >;
+  };
+}
+
+export interface MarketCatalystsCatalystSection extends Schema.Component {
+  collectionName: 'components_market_catalysts_catalyst_sections';
+  info: {
+    displayName: 'Catalyst-Section';
+    icon: 'feather';
+  };
+  attributes: {
+    Title: Attribute.String;
+    description: Attribute.RichText &
+      Attribute.CustomField<
+        'plugin::ckeditor.CKEditor',
+        {
+          output: 'HTML';
+          preset: 'rich';
+        }
+      >;
+    catalyst_items: Attribute.Component<
+      'market-catalysts.cataysts-items',
+      true
+    >;
+  };
+}
+
+export interface MarketCatalystsCatalystItem extends Schema.Component {
+  collectionName: 'components_market_catalysts_catalyst_items';
+  info: {
+    displayName: 'Catalyst Item';
+    icon: 'manyWays';
+  };
+  attributes: {
+    type: Attribute.Enumeration<
+      [
+        'Market Catalysts',
+        'Market Movers',
+        'Earnings',
+        'FDA',
+        'Economic',
+        'Conference',
+        'IPO',
+        'Dividend',
+        'Splits',
+        'Other'
+      ]
+    >;
+    date: Attribute.Date;
+    time: Attribute.Time;
+    companyEventName: Attribute.String;
+    tickerSymbol: Attribute.String;
+    description: Attribute.RichText;
+    potentialImpact: Attribute.String;
+    relatedStockSectors: Attribute.String;
+    URL: Attribute.String;
+    importanceLevel: Attribute.Enumeration<
+      [
+        'Extremely High - Stock Market Mover',
+        'High - Individual Stock Mover',
+        'Medium - Potential Stock Mover',
+        'Low - Small Chance Stock Mover'
+      ]
+    >;
+    chartImage: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    estimatedValue: Attribute.Decimal;
+    analystsConsensus: Attribute.String;
+    isConfirmed: Attribute.Boolean;
+    tags: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'market-catalysts.cataysts-items': MarketCatalystsCataystsItems;
-      'market-catalysts.catalyst-section': MarketCatalystsCatalystSection;
-      'market-catalysts.catalyst-item': MarketCatalystsCatalystItem;
       'shared.slider': SharedSlider;
       'shared.seo': SharedSeo;
       'shared.rich-text': SharedRichText;
@@ -230,6 +227,9 @@ declare module '@strapi/types' {
       'shared.meta-social': SharedMetaSocial;
       'shared.members': SharedMembers;
       'shared.media': SharedMedia;
+      'market-catalysts.cataysts-items': MarketCatalystsCataystsItems;
+      'market-catalysts.catalyst-section': MarketCatalystsCatalystSection;
+      'market-catalysts.catalyst-item': MarketCatalystsCatalystItem;
     }
   }
 }
